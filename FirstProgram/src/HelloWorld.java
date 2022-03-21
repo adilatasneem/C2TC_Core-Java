511a4d9294c9c8ac0e06787e14a6990619d51d5b
adilatasneem <adilatasneem08@gmail.com>
2022-03-21 10:13:25 +0530

    First Commit

---
+++ FirstProgram/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to C2TC Program");
+		System.out.println("This is the basic java program");
+		System.out.println("Happy  learning");
+
+
+	}
+
+}
