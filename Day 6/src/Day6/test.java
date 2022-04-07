3b351326a05d2c7189a66ef387fe98acc3d33eb8
adilatasneem <adilatasneem08@gmail.com>
2022-04-08 01:18:33 +0530

    First commit

---
+++ Day 6/src/Day6/test.java
@@ -0,0 +1,14 @@
+package Day6;
+
+public class test {
+	
+public static void main(String args[]) {
+	float b=8,h=15,area;
+	area =(b*h)/2;
+	System.out.println("Area of Triangle is:"+area);
+}
+
+public class AreaOfTriangle {
+
+}
+}
