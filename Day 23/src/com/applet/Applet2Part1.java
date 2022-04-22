6a6350b2cbfc1105fbebdd337cfe8fed7f0d3820
adilatasneem <adilatasneem08@gmail.com>
2022-04-22 13:43:33 +0530

    First commit

---
+++ Day 23/src/com/applet/Applet2Part1.java
@@ -0,0 +1,25 @@
+package com.applet;
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Applet2Part1 extends Applet implements ActionListener{
+Button b1,b2;
+TextField t;
+public void init() {
+add(b1= new Button("Click"));
+add(t = new TextField(15));
+add(b2= new Button("Clear"));
+b1.addActionListener(this);
+b2.addActionListener(this);
+}
+public void actionPerformed(ActionEvent e) {
+// TODO Auto-generated method stub
+Object o = e.getSource();
+if(o.equals(b1))
+t.setText("Hello welcome to core java");
+if(o.equals(b2))
+t.setText("");
+}
+}
