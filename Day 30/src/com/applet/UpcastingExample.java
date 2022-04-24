3114d4ce8d4c656283948e13f8da43c408434ef7
adilatasneem <adilatasneem08@gmail.com>
2022-04-25 02:29:41 +0530

    First commit

---
+++ Day 30/src/com/applet/UpcastingExample.java
@@ -0,0 +1,24 @@
+package com.applet;
+
+class  Parent{  
+	   void PrintData() {  
+	      System.out.println("method of parent class");  
+	   }  
+	}  
+	  
+	class Child extends Parent {  
+	   void PrintData() {  
+	      System.out.println("method of child class");  
+	   }  
+	}  
+	class UpcastingExample{  
+	   public static void main(String args[]) {  
+	        
+	      Parent obj1 = (Parent) new Child();  
+	      Parent obj2 = (Parent) new Child();  
+	      Parent obj3 = (Parent) new Child();
+	      obj1.PrintData();  
+	      obj2.PrintData();  
+	      obj3.PrintData(); 
+	   }  
+	}  
