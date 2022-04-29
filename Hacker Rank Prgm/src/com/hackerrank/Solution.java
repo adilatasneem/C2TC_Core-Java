f3dc87ffa912cec767c1a981fabbefe5434242ed
adilatasneem <adilatasneem08@gmail.com>
2022-04-29 09:43:18 +0530

    First commit

---
+++ Hacker Rank Prgm/src/com/hackerrank/Solution.java
@@ -0,0 +1,11 @@
+package com.hackerrank;
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    	System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    	
+    }
+}
